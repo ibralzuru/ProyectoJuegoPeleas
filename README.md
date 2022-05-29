646e4b2423134942725bcd3ccfa648492cb44877
ibralzuru <ibracortes@gmail.com>
2022-05-29 18:18:46 +0200

    jordan vs curry Readme

---
+++ README.md
@@ -0,0 +1,58 @@
+# Jordan Vs Curry  🚀
+_Este proyecto es un mini juego realizado con el metodo SPA (Single Page Aplication)._
+
+***
+
+### Pre-requisitos 📋
+_Se puede ver en cualquier navegador web._
+***
+### Instalación 🔧
+
+_Solo tienes que hacer click en el siguiente link....._
+
+
+***
+### Pruebas ⚙️
+
+_He dividido el juego en 3 pantallas:_
+
+<img src="img/readme3.png" alt="imagenWebMovil">
+
+_En esta pantalla se ven las letras del juego y también se accede a la cruenta batalla._
+
+<img src="img/readme2.png" alt="imagenWebMovil">
+
+
+_Aquí se realiza el enfrentamiento.
+Solo tiene que quemar el botón Fight y esperar el resultado._
+
+<img src="img/readme1.png" alt="imagenWebMovil">
+
+_En esta última pantalla se termina el juego y ves el nombre del ganador!._
+
+***
+
+### Para esta web utilice los siguientes lenguajes:🛠️
+
+* Html5
+* Css
+* JavaScript
+
+
+
+***
+
+
+### Autor ✒️
+
+Ibralzuru
+
+
+## Mención Especial para: 🎁
+
+* La Clase Geeks Barcelona 🍺 
+* Los profesores 📢
+* etc..... 🤓
+
+
+## Con ❤️ Ibra Alzuru C.
---
+++ js/main.js
@@ -0,0 +1,113 @@
+
+//refrescar pantalla
+
+
+const cleanGame = () => {
+
+  window.location.reload();
+
+  shiftScreen(2);
+}
+
+// Funcion que cambia la pantalla
+
+const shiftScreen = (conversion) => {
+
+  let toChoose = "pantalla" + conversion;
+
+
+  let allScreen = ["pantalla1", "pantalla2", "pantalla3"];
+
+  allScreen = allScreen.filter(val => !toChoose.includes(val));
+
+  document.getElementById(toChoose).style.display = "block";
+
+  for (let sH of allScreen) {
+    document.getElementById(sH).style.display = "none";
+  }
+
+
+}
+
+//Funcion numero random
+const funcion = {
+
+  random(min, max) {
+
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+}
+
+
+//Declaro las class
+
+class jugador {
+
+  constructor(name, id, life, luck) {
+    this.name = name;
+    this.id = id;
+    this.life = 100;
+    this.luck = funcion.random(1, 10);
+  }
+
+  atack() {
+
+    this.life -= this.luck;
+
+    this.luck = funcion.random(1, 10);
+
+  }
+};
+
+//Instanciando de jugadores
+
+let jugador1 = new jugador("jordan", "1", 100, 100, 1);
+let jugador2 = new jugador("curry", "2", 100, 100, 1);
+
+//Funcion que genera el juego
+
+const game = () => {
+
+  jugador1.atack();
+  jugador2.atack();
+
+
+
+  if (jugador1.life <= 0) {
+
+    shiftScreen(3)
+    contenedor3.innerHTML = `<h1>Stephen Curry!</h1>`;
+    /*   contenedor3.innerHTML = `<img src="../img/curryW.jpeg" alt="">`;
+      contenedor4.innerHTML = `<img src="../img/jordanC.jpeg" alt="">`;  */
+
+
+  }
+  if (jugador2.life <= 0) {
+
+    shiftScreen(3)
+    contenedor3.innerHTML = `<h1>Michael Jordan!</h1>`;
+    /*  contenedor3.innerHTML = `<img src="../img/curryC4.jpeg" alt="">`; 
+     contenedor4.innerHTML = `<img src="../img/jordanW.jpeg" alt="">`; */
+  }
+
+  power1.innerHTML = `<div class="barra-vida-interna"><div id="power1"></div>`;
+  power2.innerHTML = `<div class="barra-vida-interna"><div id="power2"></div>`;
+  document.getElementById("power1").style.width = `${jugador1.life}` + "%";
+  document.getElementById("power2").style.width = `${jugador2.life}` + "%";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
